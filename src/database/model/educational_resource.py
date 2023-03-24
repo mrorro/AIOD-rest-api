7e6bc0585caf3be95e419aa5df9339b599f459e4
Antonis Ganios <you@example.com>
2023-03-24 14:44:56 +0200

    initial commit for educational resource model

---
+++ src/database/model/educational_resource.py
@@ -0,0 +1,49 @@
+from sqlite3 import Date
+import typing  # noqa:F401 (flake8 raises incorrect 'Module imported but unused' error)
+from sqlalchemy.sql import func
+from sqlalchemy import ForeignKey, Table, Column, String, Integer, DateTime, Boolean, Interval
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from database.model.base import Base
+
+
+class EducationalResource(Base):
+    """Any educational resource"""
+
+    __tablename__ = "educational_resources"
+
+    # required fields
+    title: Mapped[str] = mapped_column(String(500), nullable=False)
+    date_modified: Mapped[Date] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    body: Mapped[str] = mapped_column(String(500), nullable=False)
+    website_url: Mapped[str] = mapped_column(String(500), nullable=False)
+    educational_role: Mapped[str] = mapped_column(String(500), nullable=False)
+    educational_level: Mapped[str] = mapped_column(String(500), nullable=False)
+    educatonal_type: Mapped[str] = mapped_column(String(500), nullable=False)
+    pace: Mapped[str] = mapped_column(String(500), nullable=False)
+
+    #optional fields
+    title: Mapped[str] = mapped_column(String(500), nullable=True)
+    interactivity_type: Mapped[str] = mapped_column(String(500), nullable=True)
+    accessibility_api: Mapped[str] = mapped_column(String(500), nullable=True)
+    accessibility_control: Mapped[str] = mapped_column(String(500), nullable=True)
+    access_mode: Mapped[str] = mapped_column(String(500), nullable=True)
+    access_restrictions: Mapped[str] = mapped_column(String(500), nullable=True)
+    citation: Mapped[str] = mapped_column(String(500), nullable=True)
+    version: Mapped[str] = mapped_column(String(500), nullable=True)
+    field_prerequisites: Mapped[str] = mapped_column(String(500), nullable=True)
+    short_summary: Mapped[str] = mapped_column(String(500), nullable=True)
+    
+    duration_minutes_and_hours: Mapped[str] = mapped_column(String(500), nullable=True)
+    hours_per_week: Mapped[str] = mapped_column(String(500), nullable=True)
+    country: Mapped[str] = mapped_column(String(500), nullable=True)
+
+
+
+    is_accessible_for_free: Mapped[Boolean] = mapped_column(Boolean, nullable=True)
+    duration_in_years: Mapped[Boolean] = mapped_column(Boolean, nullable=True)
+
+    time_required: Mapped[Interval] = mapped_column(Interval, nullable=True)
+
+
+    
+    id: Mapped[int] = mapped_column(init=False, primary_key=True)
