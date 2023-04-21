210edac6220b648d0ac48e2d0c33cc6b8522e97e
Jos van der Velde <j.d.v.d.velde@tue.nl>
2023-04-21 11:46:32 +0200

    Documentation

--- src/converters/schema_converters/schema_converter.py
+++ src/converters/schema_converters/schema_converter.py
@@ -8,6 +8,13 @@ SCHEMA_CLASS = TypeVar("SCHEMA_CLASS")
 
 
 class SchemaConverter(abc.ABC, Generic[AIOD_CLASS, SCHEMA_CLASS]):
+    """Converting between our AIoD representation of a resource and another representation. For
+    example, for dataset, we can convert it to schema.org or DCAT-AP formats.
+
+    This makes it easier for other services to connect to AIoD. Instead of adapting to the AIoD
+    schema, they can use existing standards.
+    """
+
     @property
     @abc.abstractmethod
     def to_class(self) -> Type[SCHEMA_CLASS]:
