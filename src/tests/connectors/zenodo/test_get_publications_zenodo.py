23a7b295a3ef64015f2c1a9878b0b549dfc1b03b
Íñigo Aréjula Aísa <arejula27@gmail.com>
2023-05-25 10:45:16 +0200

    delete test useless definitions

--- src/tests/connectors/zenodo/test_get_publications_zenodo.py
+++ src/tests/connectors/zenodo/test_get_publications_zenodo.py
@@ -17,38 +17,24 @@ class MockRecord:
         self.raw = raw
 
 
-def mock_list_records(**kwargs):
-    # Aquí puedes simular los registros que deseas devolver
-    # como si estuvieran siendo devueltos por Sickle
-    mock_records = [
-        MockRecord(
-            read_file(path_test_resources() / "connectors/zenodo/article_example.xml")
-        ),  # Ejemplo de registro 1
-        MockRecord(
-            read_file(path_test_resources() / "connectors/zenodo/dataset_example.xml")
-        ),  # Ejemplo de registro 2
-    ]
-    return mock_records
 
+class MockSickle:
+        def ListRecords(self, ):
+            mock_records = [
+                MockRecord(
+                    read_file(path_test_resources() / "connectors/zenodo/article_example.xml")
+            ),
+            MockRecord(
+                read_file(path_test_resources() / "connectors/zenodo/dataset_example.xml")
+            ),  
+            ]
+            return mock_records
 
-def mock_sickle_ListRecords(*args, **kwargs):
-    return mock_list_records(**kwargs)
-
-
-def mock_sickle(**kwargs):
-    class MockSickle:
-        def __init__(self, *args, **kwargs):
-            pass
-
-        def ListRecords(self, *args, **kwargs):
-            return mock_sickle_ListRecords(*args, **kwargs)
-
-    return MockSickle(**kwargs)
 
 
 def test_process_dataset_record():
     connector = cast(ZenodoDatasetConnector, connectors.dataset_connectors[PlatformName.zenodo])
-    sickle = mock_sickle()
+    sickle = MockSickle()
     date = datetime(2023, 5, 24, 5, 0, 0)
     datasets = list(connector._retrieve_dataset_from_datetime(sickle, date))
     assert len(datasets) == 1
