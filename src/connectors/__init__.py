29b9ce14dcf7aa93acf3dd52e7d18936de2d6951
Jos van der Velde <j.d.v.d.velde@tue.nl>
2023-05-29 10:37:02 +0200

    Updated connectors.__init__

--- src/connectors/__init__.py
+++ src/connectors/__init__.py
@@ -1,17 +1,12 @@
-import typing  # noqa:F401 (flake8 raises incorrect 'Module imported but unused' error)
-
-import platform_names  # noqa:F401
-from database.model.dataset import Dataset  # noqa:F401
 from .abstract.resource_connector import ResourceConnector  # noqa:F401
 from .example.example_dataset_connector import ExampleDatasetConnector
+from .example.example_publication_connector import ExamplePublicationConnector
 from .huggingface.huggingface_dataset_connector import HuggingFaceDatasetConnector
 from .openml.openml_dataset_connector import OpenMlDatasetConnector
 
 dataset_connectors = {
     c.platform_name: c
     for c in (ExampleDatasetConnector(), OpenMlDatasetConnector(), HuggingFaceDatasetConnector())
-}  # type: typing.Dict[platform_names.PlatformName, ResourceConnector[Dataset]]
+}
 
-# publication_connectors = {
-#     p.platform_name: p for p in (ExamplePublicationConnector(), ZenodoPublicationConnector())
-# }  # type: typing.Dict[platform_names.PlatformName, ResourceConnector[schemas.AIoDPublication]]
+publication_connectors = {p.platform_name: p for p in [ExamplePublicationConnector()]}
