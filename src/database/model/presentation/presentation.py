5a9bc22d161e5b9d3009b34c7367d2f41c3b4a46
Íñigo Aréjula Aísa <arejula27@gmail.com>
2023-06-02 09:57:30 +0200

    presentation

---
+++ src/database/model/presentation/presentation.py
@@ -0,0 +1,23 @@
+
+
+from database.model.resource import Resource
+from sqlmodel import Field
+from datetime import datetime
+
+
+class PresentationBase(Resource):
+    #Required fields
+    name: str = Field(max_length=150, schema_extra={"example": "Example Presentation"})
+
+    description:  str = Field(max_length=5000, schema_extra={"example": "A description."})
+    # Recommended fields
+    url : str| None = Field(max_length=250,default=None, schema_extra={"example": "https://example.example"})
+    datePublished: datetime | None = Field(
+        default=None, schema_extra={"example": "2022-01-01T15:15:00.000Z"}
+    )
+    publisher: str | None = Field(
+        max_length=150, default=None, schema_extra={"example": "John Doe"}
+    )
+    author:str | None = Field(max_length=250, default=None, schema_extra={"example": "John Doe"})
+    image:str | None= Field(max_length=250,default=None, schema_extra={"example": "https://example.example"})
+    is_accessible_for_free: bool = Field(default=True)
