56dbeccaad6780bfa33c87c0fa45fd667886b707
Íñigo Aréjula Aísa <arejula27@gmail.com>
2023-06-05 13:27:45 +0000

    super and sub events

--- src/database/model/event/event.py
+++ src/database/model/event/event.py
@@ -1,6 +1,15 @@
 from datetime import datetime
-from sqlmodel import Field
+from sqlmodel import Field, Relationship, SQLModel
+from typing import List
+from database.model.relationships import ResourceRelationshipList
 from database.model.resource import Resource
+from serialization import AttributeSerializer, FindByIdentifierDeserializer
+
+
+class EventParentChildLink(SQLModel, table=True):  # type: ignore [call-arg]
+    __tablename__ = "event_parent_child_link"
+    parent_identifier: int = Field(foreign_key="event.identifier", primary_key=True)
+    child_identifier: int = Field(foreign_key="event.identifier", primary_key=True)
 
 
 class EventBase(Resource):
@@ -16,14 +25,54 @@ class EventBase(Resource):
         default=None, schema_extra={"example": "2022-01-01T15:15:00"}
     )
     end_date: datetime | None = Field(default=None, schema_extra={"example": "2022-01-01T15:15:00"})
-    duration: str = Field(max_length=500, schema_extra={"example": "Example duration Event"})
-    status: str = Field(max_length=500, schema_extra={"example": "Example status Event"})
-    attendance_mode: str = Field(
-        max_length=500, schema_extra={"example": "Example attendance mode Event"}
+    duration: str | None = Field(
+        default=None, max_length=500, schema_extra={"example": "Example duration Event"}
     )
-    type: str = Field(max_length=500, schema_extra={"example": "Example type Event"})
+    status: str | None = Field(
+        default=None, max_length=500, schema_extra={"example": "Example status Event"}
+    )
+    attendance_mode: str | None = Field(
+        default=None, max_length=500, schema_extra={"example": "Example attendance mode Event"}
+    )
+    type: str | None = Field(
+        default=None, max_length=500, schema_extra={"example": "Example type Event"}
+    )
 
 
 class Event(EventBase, table=True):  # type: ignore [call-arg]
     __tablename__ = "event"
     identifier: int = Field(primary_key=True, foreign_key="ai_asset.identifier")
+    # This entity is implemented in the pr 52, it atributes will be implemented after merging it
+    # TODO add business_categories
+    sub_events: List["Event"] = Relationship(
+        back_populates="super_events",
+        link_model=EventParentChildLink,
+        sa_relationship_kwargs=dict(
+            primaryjoin="Event.identifier==EventParentChildLink.parent_identifier",
+            secondaryjoin="Event.identifier==EventParentChildLink.child_identifier",
+        ),
+    )
+    super_events: List["Event"] = Relationship(
+        back_populates="sub_events",
+        link_model=EventParentChildLink,
+        sa_relationship_kwargs=dict(
+            primaryjoin="Event.identifier==EventParentChildLink.child_identifier",
+            secondaryjoin="Event.identifier==EventParentChildLink.parent_identifier",
+        ),
+    )
+
+    class RelationshipConfig:
+        sub_events: List[int] = ResourceRelationshipList(
+            example=[3, 4],
+            serializer=AttributeSerializer("identifier"),
+        )
+        super_events: List[int] = ResourceRelationshipList(
+            example=[1, 2],
+            serializer=AttributeSerializer("identifier"),
+        )
+
+
+# Defined separate because it references Event, and can therefor not be defined inside Dataset
+deserializer = FindByIdentifierDeserializer(Event)
+Event.RelationshipConfig.super_events.deserializer = deserializer  # type: ignore[attr-defined]
+Event.RelationshipConfig.sub_events.deserializer = deserializer  # type: ignore[attr-defined]
--- src/tests/routers/test_router_event.py
+++ src/tests/routers/test_router_event.py
@@ -1,15 +1,34 @@
 from unittest.mock import Mock
 
 from starlette.testclient import TestClient
-
+from sqlalchemy.orm import Session
+from sqlalchemy.engine import Engine
 from authentication import keycloak_openid
+from database.model.ai_asset import AIAsset
+from database.model.event.event import Event
 
 
-def test_happy_path(client: TestClient, mocked_privileged_token: Mock):
+def test_happy_path(client: TestClient, engine: Engine, mocked_privileged_token: Mock):
     keycloak_openid.decode_token = mocked_privileged_token
+    with Session(engine) as session:
+        session.add_all(
+            [
+                AIAsset(type="event"),
+                Event(
+                    identifier="1",
+                    name="Parent",
+                    platform="example",
+                    platform_identifier="1",
+                    description="description text",
+                    registration_url="https://example.com/event/example/registration",
+                    location="Example location Event",
+                ),
+            ]
+        )
+        session.commit()
     body = {
-        "platform": "zenodo",
-        "platform_identifier": "1",
+        "platform": "example",
+        "platform_identifier": "2",
         "name": "Example Event",
         "description": "example descriprion",
         "registration_url": "https://example.com/event/example/registration",
@@ -20,17 +39,19 @@ def test_happy_path(client: TestClient, mocked_privileged_token: Mock):
         "status": "Example status Event",
         "attendance_mode": "Example attendance mode Event",
         "type": "Example type Event",
+        "super_events": [1],
+        "sub_events": [],
     }
     response = client.post("/events/v0", json=body, headers={"Authorization": "Fake token"})
     assert response.status_code == 200
 
-    response = client.get("/events/v0/1")
+    response = client.get("/events/v0/2")
     assert response.status_code == 200
 
     response_json = response.json()
-    assert response_json["identifier"] == 1
-    assert response_json["platform"] == "zenodo"
-    assert response_json["platform_identifier"] == "1"
+    assert response_json["identifier"] == 2
+    assert response_json["platform"] == "example"
+    assert response_json["platform_identifier"] == "2"
     assert response_json["name"] == "Example Event"
     assert response_json["description"] == "example descriprion"
     assert response_json["registration_url"] == "https://example.com/event/example/registration"
@@ -41,3 +62,4 @@ def test_happy_path(client: TestClient, mocked_privileged_token: Mock):
     assert response_json["status"] == "Example status Event"
     assert response_json["attendance_mode"] == "Example attendance mode Event"
     assert response_json["type"] == "Example type Event"
+    assert set(response_json["super_events"]) == {1}
