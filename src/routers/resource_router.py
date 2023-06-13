97d1f423c11b1f8e451f32f809d9b7e379aa5b51
mtkinit <martin.tamajka@kinit.sk>
2023-06-13 13:22:01 +0200

    Added 'get resource count' endpoint to resource_router

--- src/routers/resource_router.py
+++ src/routers/resource_router.py
@@ -123,6 +123,13 @@ class ResourceRouter(abc.ABC):
             **default_kwargs,
         )
         router.add_api_route(
+            path=f"{url_prefix}/count/{self.resource_name_plural}/{version}",
+            endpoint=self.get_resource_count_func(engine),
+            response_model=int,  # type: ignore
+            name=f"Count of {self.resource_name_plural}",
+            **default_kwargs,
+        )
+        router.add_api_route(
             path=f"{url_prefix}/{self.resource_name_plural}/{version}",
             methods={"POST"},
             endpoint=self.register_resource_func(engine),
@@ -230,6 +237,23 @@ class ResourceRouter(abc.ABC):
             return resources
 
         return get_resources
+    
+    def get_resource_count_func(self, engine: Engine):
+        """
+        Gets the total number of resources from the database.
+        This function returns a function (instead of being that function directly) because the
+        docstring and the variables are dynamic, and used in Swagger.
+        """
+
+        def get_resource_count():
+            f"""Retrieve the number of {self.resource_name_plural}."""
+            try:
+                with Session(engine) as session:
+                    return session.query(self.resource_class).count()
+            except Exception as e:
+                raise _wrap_as_http_exception(e)
+
+        return get_resource_count
 
     def get_platform_resources_func(self, engine: Engine):
         """
