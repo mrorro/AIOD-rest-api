064391b571880b8d7afb32d4f46c3e72ecb0fddb
Íñigo Aréjula Aísa <arejula27@gmail.com>
2023-06-16 12:13:34 +0000

    new test

--- src/tests/uploader/huggingface/test_dataset_uploader.py
+++ src/tests/uploader/huggingface/test_dataset_uploader.py
@@ -65,4 +65,54 @@ def test_happy_path_new_repository(
 #  code works)
 
 
+def test_repo_already_exists(client: TestClient, engine: Engine, mocked_privileged_token: Mock):
+    keycloak_openid.decode_token = mocked_privileged_token
+    dataset_id = 1
+    with Session(engine) as session:
+        session.add_all(
+            [
+                AIAsset(type="dataset"),
+                Dataset(
+                    identifier=dataset_id,
+                    name="Parent",
+                    platform="example",
+                    platform_identifier="1",
+                    description="description text",
+                    same_as="",
+                ),
+            ]
+        )
+        session.commit()
+
+    data = {
+        "token": "huggingface_token",
+        "username": "username",
+    }
+
+    with open(path_test_resources() / "uploaders" / "huggingface" / "example.csv", "rb") as f:
+        files = {"file": f.read()}
+
+    with responses.RequestsMock() as mocked_requests:
+        mocked_requests.add(
+            responses.POST,
+            "https://huggingface.co/api/repos/create",
+            json={
+                "error": "You already created this dataset repo",
+                "url": "url",
+            },
+            status=409,
+        )
+        huggingface_hub.upload_file = Mock(return_value=None)
+        response = client.post(
+            f"/upload/datasets/{dataset_id}/huggingface",
+            data=data,
+            params={"username": "Fake-username", "token": "Fake-token"},
+            headers={"Authorization": "Fake token"},
+            files=files,
+        )
+    assert response.status_code == 200
+    id_response = response.json()
+    assert id_response == dataset_id
+
+
 # TODO: tests some error handling?
